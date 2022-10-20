cff04f7dc0def546476aa26d031d695e37c31d68
SammyLam <sammylam_1@hotmail.com>
2022-10-20 15:25:50 -0400

    media queries

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const button = document.querySelector(".menu-collapsed");
+const button = document.querySelector('.menu-collapsed');
 
-button.addEventListener("click", () => {
-  button.classList.toggle("menu-expanded");
+button.addEventListener('click', () => {
+	button.classList.toggle('menu-expanded');
 });
 
 function contact(event) {
-  event.preventDefault();
-
-  emailjs
-    .sendForm(
-      "service_1xou5c7",
-      "template_p68bvop",
-      event.target,
-      "8X5aCUiGHuFKNdN71"
-    )
-    .then(() => {
-      console.log("hello world");
-      loading();
-    })
-    .catch(() => {
-      alert(
-        "email is temporarily unavailable. Please contact me on Sammylam505@gmail.com"
-      );
-    });
+	event.preventDefault();
+	const thanks = document.getElementById('thankYou');
+	const form = document.getElementById('form');
+	emailjs
+		.sendForm(
+			'service_1xou5c7',
+			'template_p68bvop',
+			event.target,
+			'8X5aCUiGHuFKNdN71'
+		)
+		.then(() => {
+			form.classList.remove('visable');
+			thanks.classList.remove('visuallyHidden');
+		})
+		.catch(() => {
+			form.classList.remove('visable');
+			alert(
+				'Email Service is temporarily unavailable. Please contact me on Sammylam505@gmail.com'
+			);
+		});
 }
