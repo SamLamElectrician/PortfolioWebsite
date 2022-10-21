2174f7bcf0c7d3bc51fead4b2adf562ef59e1315
SammyLam <sammylam_1@hotmail.com>
2022-10-20 20:14:55 -0400

    working form

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ button.addEventListener('click', () => {
 
 function contact(event) {
 	event.preventDefault();
-	const thanks = document.getElementById('thankYou');
-	const form = document.getElementById('form');
+	const thanks = document.querySelector('.thankYou');
+	const form = document.getElementById('contact');
+	const spinner = document.getElementById('spinner');
+	form.classList.add('visuallyHidden');
+	spinner.style.display = 'block';
+
 	emailjs
 		.sendForm(
 			'service_1xou5c7',
@@ -16,11 +20,12 @@ function contact(event) {
 			'8X5aCUiGHuFKNdN71'
 		)
 		.then(() => {
-			form.classList.remove('visable');
-			thanks.classList.remove('visuallyHidden');
+			// form.classList.add('visuallyHidden');
+			spinner.style.display = 'none';
+			thanks.style.display = 'block';
 		})
 		.catch(() => {
-			form.classList.remove('visable');
+			form.classList.add('visable');
 			alert(
 				'Email Service is temporarily unavailable. Please contact me on Sammylam505@gmail.com'
 			);
